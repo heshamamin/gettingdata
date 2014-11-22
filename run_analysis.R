77d5c27a4388e20eddb5f8f9cc194aff2394102d
Hesham A. Amin <hspceng@gmail.com>
2014-11-22 20:55:00 +0200

    Point #4

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,3 @@
-#import data table library used by this script
-library(data.table)
-
 #Read column names and extract the ones with "mean" or "std" in their names
 #Used later to get only the required columns
 column_names <- read.table("UCI HAR Dataset/features.txt")
@@ -13,12 +10,14 @@ activity_lables <- read.table("UCI HAR Dataset/activity_labels.txt")
 
 #read training set, subjects of trainig set ,and labels of training set
 train_in <- read.table(file = "UCI HAR Dataset/train/X_train.txt", header = F)
+names(train_in) <- column_names$V2
 subject_train <- read.table(file = "UCI HAR Dataset/train/subject_train.txt", header = F,col.names = c("subject"))
 label_train <- read.table(file = "UCI HAR Dataset/train/y_train.txt", header = F,col.names = c("activityid"))
 train <- cbind(subject_train, label_train, train_in)
 
 #read test set, subjects of test set ,and labels of test set
 test_in <- read.table(file = "UCI HAR Dataset/test/X_test.txt", header = F)
+names(test_in) <- column_names$V2
 subject_test <- read.table(file = "UCI HAR Dataset/test/subject_test.txt", header = F,col.names = c("subject"))
 label_test <- read.table(file = "UCI HAR Dataset/test/y_test.txt", header = F,col.names = c("activityid"))
 test <- cbind(subject_test, label_test, test_in)
@@ -32,15 +31,29 @@ merged_all <- rbind(train, test)
 merged <- merged_all[, c(T,T,mean_column_names | std_column_names)]
 
 #3.Uses descriptive activity names to name the activities in the data set
-merged$activity <- factor(merged$activityid, labels = activity_lables$V2)
-merged$activityid <- NULL
+merged$activityid <- factor(merged$activityid, labels = activity_lables$V2)
+colnames(merged)[2] <- "activity"
 
 
-
-#add subjects and labels to merged data sets, and convert to data table for next steps
-merged_data <- cbind(merged_subjects, merged_lables, merged_data)
-merged_data <- as.data.table(merged_data)
-
-#add redable activity label names
-merged_data <- merged_data[, activity:=activity_lables[activity_label_id,2]]
-
+#4.Appropriately labels the data set with descriptive variable names.
+#Using style similar to Google's R Style Guide: https://google-styleguide.googlecode.com/svn/trunk/Rguide.xml
+all.names <- names(merged)
+#use . instead of camelCase, and replace "t" with "time"
+all.names <- gsub(pattern = "tBody", replacement = "time.body", x=all.names)
+all.names <- gsub(pattern = "tGravity", replacement = "time.gravity", x=all.names)
+#use . instead of camelCase, and replace "f" with "frequency"
+all.names <- gsub(pattern = "fBody", replacement = "frequency.body", x=all.names) #
+#replace bodyBody with "body"
+all.names <- gsub(pattern = "bodyBody", replacement = "body", x=all.names)
+#replace abbreviations
+all.names <- gsub(pattern = "Gyro", replacement = ".gyroscope", x=all.names)
+all.names <- gsub(pattern = "Jerk", replacement = ".jerk", x=all.names)
+all.names <- gsub(pattern = "Mag", replacement = ".magnitude", x=all.names)
+all.names <- gsub(pattern = "Acc", replacement = ".acceleration", x=all.names)
+#replace special characters
+all.names <- gsub(pattern = "\\(\\)", replacement = "", x=all.names)
+all.names <- gsub(pattern = "-", replacement = ".", x=all.names)
+all.names <- gsub(pattern = "meanFreq", replacement = "mean.frequecy", x=all.names)
+#all lower case
+all.names <- tolower(all.names)
+names(merged) <- all.names
